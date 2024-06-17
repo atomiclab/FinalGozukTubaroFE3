36ee9064a7b9e388a7cf63a37a73d7f8e6bfa4e3
atomiclab <ginotubaro@gmail.com>
2024-06-17 00:34:44 -0300

    agregado localstorage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Card from "../Components/Card";
 import ThemeContext, { themes } from "../Context/theme.context";
 import Button from "../Components/Button";
 import { useRecipeStates } from "../Context/global.context";
-import "../Styles/Cards.css";
+import "../Styles/Cards.css"
+
 
 const Home = () => {
   const { theme, handleChangeTheme } = useContext(ThemeContext);
   const { state, dispatch } = useRecipeStates();
 
   const handleFavoriteClick = (recipe) => {
-    const isFavorite = state.favorites.some((fav) => fav.id === recipe.id);
+    const isFavorite = state.favorites.some(fav => fav.id === recipe.id);
     if (isFavorite) {
       dispatch({ type: "REMOVE_FAVORITE", payload: recipe });
     } else {
       dispatch({ type: "ADD_FAVORITE", payload: recipe });
     }
   };
+  useEffect(() => {
+    dispatch({ type: "INIT_FAVORITES" });
+  }, []);
 
   return (
     <main className="">
@@ -25,9 +29,7 @@ const Home = () => {
       <div className="card-grid">
         <div className="list-container">
           {state.recipes.map((recipe) => {
-            const isFavorite = state.favorites.some(
-              (fav) => fav.id === recipe.id
-            );
+            const isFavorite = state.favorites.some(fav => fav.id === recipe.id);
             return (
               <Card item={recipe} key={recipe.id}>
                 <Button onClick={() => handleFavoriteClick(recipe)}>
--- src/Reducers/Reducer.js
+++ src/Reducers/Reducer.js
@@ -1,7 +1,7 @@
 export const reducer = (state, action) => {
   switch (action.type) {
     case "GET_RECIPES":
-      return { ...state, recipes: action.payload }; // Asegúrate de que payload tenga el formato correcto
+      return { ...state, recipes: action.payload };
     // case "ADD_CART":
     //   return { ...state, cart: [...state.cart, action.payload] };
     // case "DELETE_CART":
@@ -12,16 +12,31 @@ export const reducer = (state, action) => {
 
     case "ADD_FAVORITE":
       console.log(action.payload);
-       return { ...state, favorites: [...state.favorites, action.payload] };
-      //return { ...state, favorites: action.payload };
+      const newFavoritesLocalStorage = [...state.favorites, action.payload];
+      localStorage.setItem(
+        "favorites",
+        JSON.stringify(newFavoritesLocalStorage)
+      );
+      return { ...state, favorites: [...state.favorites, action.payload] };
+    //return { ...state, favorites: action.payload };
     case "REMOVE_FAVORITE":
+      const newFavoritesRemove = state.favorites.filter(
+        (doc) => doc.id !== action.payload.id
+      );
+      localStorage.setItem("favorites", JSON.stringify(newFavoritesRemove));
+
       return {
         ...state,
         favorites: state.favorites.filter(
           (doc) => doc.id !== action.payload.id
         ),
       };
-
+    case "INIT_FAVORITES":
+      const storedFavorites = localStorage.getItem("favorites");
+      return {
+        ...state,
+        favorites: storedFavorites ? JSON.parse(storedFavorites) : [],
+      };
     default:
       throw new Error();
   }
